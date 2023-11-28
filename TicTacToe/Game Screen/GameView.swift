1767665c151845bd406b6b0e323ac9bc57a1dcc4
Deekshu Devarasetty <ddevarasetty3@gmail.com>
2023-11-28 14:05:15 -0500

    Initial Commit

---
+++ TicTacToe/Game Screen/GameView.swift
@@ -0,0 +1,31 @@
+//
+//  GameView.swift
+//  TicTacToe
+//
+//  Created by Deekshu Devarasetty on 11/28/23.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    @Environment(\.dismiss) var dismiss
+    var body: some View {
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button("End Game") {
+                    dismiss()
+                }
+                .buttonStyle(.bordered)
+            }
+        }
+        .navigationTitle("Tic Tac Toe")
+        .inNavigationStack()
+    }
+}
+
+#Preview {
+    GameView()
+}
---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Deekshu Devarasetty on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct AppEntry: App {
+    var body: some Scene {
+        WindowGroup {
+            StartView()
+        }
+    }
+}
